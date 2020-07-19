93f89f161959154258c0cdb98faba35657a3c51a
Moamen <moamenzalabia@gmail.com>
2020-07-19 20:32:26 +0200

    adding sub order details table cells

---
+++ TaskOne/ViewControllers/TrackOrderVC/OrderStatusTVCell/OrderStatusDataTVCell/OrderStatusDataTVCell.swift
@@ -0,0 +1,24 @@
+//
+//  OrderStatusDataTVCell.swift
+//  TaskOne
+//
+//  Created by Moamen Abd ELgawad on 7/19/20.
+//  Copyright © 2020 Ahmed Elgamal. All rights reserved.
+//
+
+import UIKit
+
+class OrderStatusDataTVCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
